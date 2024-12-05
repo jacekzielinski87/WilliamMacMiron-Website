0a80da4c5cda7ca376483fca5f7ef8ab85c238eb
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-12-05 18:27:01 +0100

    Design

--- pages/components/Logo.jsx
+++ pages/components/Logo.jsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 const Logo = () => {
   return (
-    <div className="fixed bottom-0 left-1/2 -translate-x-1/2 z-50 overflow-visible">
+    <div className="fixed bottom-0 left-1/2 -translate-x-1/2 z-50 overflow-visible h-[15px] mb-10">
       <Link href="/" replace> {/* Added replace to replace current route in history */}
         <Image
           src="/assets/logoo.png"
           alt="Logo"
-          width={45}
-          height={125}
-          className="cursor-pointer border-2 border-black rounded-t-3xl hover:scale-100 transition-transform p-1 mx-auto"
+          width={30}
+          height={60}
+          className="cursor-pointer border-2 border-black rounded-t-3xl hover:scale-100 transition-transform p-1 mx-auto w-[70px]"
           priority
         />
       </Link>
--- pages/components/NavbarRight.jsx
+++ pages/components/NavbarRight.jsx
@@ -10,10 +10,10 @@ const NavbarRight = () => {
   return (
     <div className="bg-[#C8C8C8] h-[40px] border-2 border-black rounded-t-3xl 
       fixed bottom-0 
-      w-[460px]
+      w-[450px]
       left-1/2 transform -translate-x-1/1 
       rounded-b-none
-      flex items-center justify-around px-12 z-50 ml-5">
+      flex items-center justify-around px-12 z-50 ml-8">
       <ul className="h-full w-full justify-center">
         <li className="flex justify-evenly items-center h-full relative">
           <Link
