5a0dccc6eb1b3980156f45fa9990faa83c3fb409
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-12-05 18:34:50 +0100

    Design

--- pages/components/Logo.jsx
+++ pages/components/Logo.jsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 const Logo = () => {
   return (
-    <div className="fixed bottom-0 left-1/2 -translate-x-1/2 z-50 overflow-visible h-[15px] mb-10">
+    <div className="fixed bottom-0 left-1/2 -translate-x-1/2 z-50 overflow-visible mb-5 h-[20px]">
       <Link href="/" replace> {/* Added replace to replace current route in history */}
         <Image
           src="/assets/logoo.png"
           alt="Logo"
-          width={30}
-          height={60}
-          className="cursor-pointer border-2 border-black rounded-t-3xl hover:scale-100 transition-transform p-1 mx-auto w-[70px]"
+          width={70}
+          height={100}
+          className="cursor-pointer border-2 border-black rounded-t-3xl hover:scale-100 transition-transform p-2 h-[50px] w-[70px]"
           priority
         />
       </Link>
